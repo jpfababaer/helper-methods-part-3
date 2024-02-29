48063aa6ad8d03d8fe4d75267005016d8db932c7
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-02-29 21:51:27 +0000

    finished all challenges

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :delete]
+
   def new
     @movie = Movie.new
   end
@@ -14,7 +16,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
+
   end
 
   def create
@@ -31,11 +33,10 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
+
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
 
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -45,7 +46,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
 
     @movie.destroy
 
@@ -57,6 +57,11 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  #Run before actions: show, edit, update, destroy. All of these require to know which movie instance.
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 
 end
